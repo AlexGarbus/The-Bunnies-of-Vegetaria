c4589cff529a2b37fbe2e58172bdcb875d838541
AlexGarbus <alex.garbus@outlook.com>
2020-04-22 11:49:17 -0400

    Refactored the Area Menu a bit

--- Assets/Scripts/AreaButton.cs
+++ Assets/Scripts/AreaButton.cs
@@ -7,33 +7,17 @@ using UnityEngine.UI;
 
 public class AreaButton : MonoBehaviour
 {
-    private enum Area { LettuceFields = 1, CeleryWoods, BroccoliForest, BokChoyBluff, CarrotTop }
-
     [SerializeField] private TMP_Text buttonText;
     [SerializeField] private Image areaImage;
-    [SerializeField] private Area area;
 
-    private bool locked = false;
     private const string lockedText = "???";
-    private GameManager gameManager;
 
-    private void Start()
+    /// <summary>
+    /// Set this area button to display as locked.
+    /// </summary>
+    public void SetLocked()
     {
-        gameManager = GameManager.Instance;
-
-        if((int)area > SaveData.current.areasUnlocked)
-        {
-            locked = true;
-            buttonText.text = lockedText;
-            areaImage.color = Color.black;
-        }
-    }
-
-    public void LoadArea()
-    {
-        if(!locked)
-        {
-            // TODO: Set area in Game Manager
-        }
+        buttonText.text = lockedText;
+        areaImage.color = Color.black;
     }
 }
--- Assets/Scripts/AreaMenu.cs
+++ Assets/Scripts/AreaMenu.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+// TODO: Move Area to separate file/class/namespace
+public enum Area { LettuceFields = 1, CeleryWoods, BroccoliForest, BokChoyBluff, CarrotTop }
+
 public class AreaMenu : MonoBehaviour
 {
+    [Header("Map")]
+    [Tooltip("Buttons should be in ascending order by area.")]
+    [SerializeField] private AreaButton[] areaButtons;
+
+    [Header("Party")]
     [SerializeField] private TMP_Text statText;
 
     private const string spacer = "  ";
@@ -15,12 +24,34 @@ public class AreaMenu : MonoBehaviour
 
     private void Start()
     {
+        // Set up map buttons
+        for (int i = 0; i < areaButtons.Length; i++)
+        {
+            if (i + 1 > SaveData.current.areasUnlocked)
+                areaButtons[i].SetLocked();
+        }
+
         // TODO: Store in Game Manager instead
         SaveData save = SaveData.current;
         bunnight = new Bunny(BunnyType.Bunnight, save.bunnightName, save.bunnightExp);
         bunnecromancer = new Bunny(BunnyType.Bunnecromancer, save.bunnecromancerName, save.bunnecromancerExp);
         bunnurse = new Bunny(BunnyType.Bunnurse, save.bunnurseName, save.bunnurseExp);
         bunneerdowell = new Bunny(BunnyType.Bunneerdowell, save.bunneerdowellName, save.bunneerdowellExp);
+    }
+
+    /// <summary>
+    /// Load the selected area if it is unlocked.
+    /// </summary>
+    /// <param name="areaName">The name of the area to load.</param>
+    public void LoadArea(string areaName)
+    {
+        Area area;
+        areaName = areaName.Replace(" ", string.Empty);
+
+        if (Enum.TryParse(areaName, false, out area) && (int)area <= SaveData.current.areasUnlocked)
+        {
+            // TODO: Load area
+        }
     }
 
     public void DisplayBunnyStats(int typeIndex)
